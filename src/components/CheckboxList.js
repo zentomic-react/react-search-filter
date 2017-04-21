fb4fe636f638cff306f9b0bc2d22331ec41a1642
zentomic <zentomic-react@outlook.com>
2017-04-20 21:50:53 -0700

    added a checkbox component

---
+++ src/components/CheckboxList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { connect } from 'react-redux'
+import { addFilter } from '../actionCreators'
+
+let CheckboxList = ({ dispatch }) => {
+	return (
+		<div>
+			<ul>
+			<input type='checkbox' name='filter1' onclick={ dispatch(addFilter()) } />Samsung<br />
+			<input type='checkbox' name='filter2' onclick={ dispatch(addFilter()) } />LG<br />
+			<input type='checkbox' name='filter3' onclick={ dispatch(addFilter()) } />Apple<br />
+			</ul>
+		</div>
+	)
+}
+
+CheckboxList = connect()(CheckboxList)
+
+export default CheckboxList	
